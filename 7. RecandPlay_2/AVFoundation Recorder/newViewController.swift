254059708640a7c02ebcfb1e9aa70e9a9bb8f909
Yun Choi <yc2519@nyu.edu>
2016-12-01 11:29:34 -0500

    Add files via upload

---
+++ 7. RecandPlay_2/AVFoundation Recorder/newViewController.swift
@@ -0,0 +1,36 @@
+//
+//  newViewController.swift
+//  Welcome
+//
+//  Created by ChoiYunho on 11/27/16.
+//  Copyright © 2016 Gene De Lisa. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class newViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
